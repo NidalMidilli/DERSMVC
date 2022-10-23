c093ae198fe67d61ac6a929abbee62ebd6e3cecf
Nidal Midilli <nidalmidilli@gmail.com>
2022-10-23 15:59:35 +0300

    Pazar Günü DERS
    Pazar Günü DERS

---
+++ DersMVC/Business/PersonelMan.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class PersonelMan
+    {
+        Repository<Personel> persrepo = new Repository<Personel>();
+
+        public List<Personel> GetAll()
+        {
+            return persrepo.Liste();
+        }
+
+        public int PersonelEkle(Personel pers)
+        {
+            if (pers.personel_ad.Length > 10)
+            {
+                return -1;
+                
+            }
+            return persrepo.Ekle(pers);
+        }
+
+        public int PersonelSil(Personel pers)
+        {
+            if(pers.personel_id == 5)
+            {
+                return persrepo.Sil(pers);
+            }
+            return -1;
+        }
+
+        public int PersonelUpdate(Personel pers)
+        {
+            if(pers.personel_id == 2)
+            {
+                return persrepo.Guncelle(pers);
+            }
+            return -1;
+        }
+
+
+    }
+}
---
+++ DersMVC/DersMVC/Controllers/PersonelController.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.Abstract;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DersMVC.Controllers
+{
+    public class PersonelController : Controller
+    {
+
+        private readonly IRepository<Personel> repository;
+
+        public PersonelController(IRepository<Personel> repository)
+        {
+            this.repository = repository;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = repository.Liste();
+            return View(result);
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Personel pers)
+        {
+            var result = repository.Ekle(pers);
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Delete(Personel pers)
+        {
+            var result = repository.Sil(pers);
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Update(Personel pers)
+        {
+            var result = repository.Guncelle(pers);
+            return View(result);
+        }
+        
+    }
+}
